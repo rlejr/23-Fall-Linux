33fb79c4a1c88a2dbbf66038420f2dc883d68277
rlejr <rlejr159753@gamil.com>
2023-11-15 16:36:11 +0900

    writer 추가

---
+++ npwriter.c
@@ -0,0 +1,28 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#define MAXLINE 100
+
+int main()
+{
+	int fd, length, i;
+	char message[MAXLINE];
+	sprintf(message, "Heelo from PID %d", getpid());
+	length = strlen(message)+1;
+	do{
+		fd = open("myPipe", O_WRONLY);
+		if(fd == -1) sleep(1);
+	} while (fd == -1);
+
+	for(i = 0; i<=3;i++){
+		write(fd,message,length);
+		sleep(3);
+	}
+	close(fd);
+	return 0;
+}
+
+
