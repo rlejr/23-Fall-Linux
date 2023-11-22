91f5f34a14eb055fb3feb44143fc9f875d0f2226
rlejr <rlejr159753@gamil.com>
2023-11-22 15:40:27 +0900

    sleep 추가

---
+++ signalHandler.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+void intHandler();
+void intHandler2();
+
+int main()
+{
+	signal(SIGINT, intHandler);
+	sleep(3);
+	raise(SIGINT);
+	printf("End of main \n");
+}
+void intHandler(int signo)
+{
+	printf("SIGINT \n");
+	printf("Sig No.:%d \n",signo);
+	exit(0);
+}
+
